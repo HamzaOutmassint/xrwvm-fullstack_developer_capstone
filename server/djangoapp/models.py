44734b08008bd597d7c7188c1205f5a5d022dddd
Hamza Outmassint <hamzaayrom@gmail.com>
2026-07-01 21:37:09 +0100

    Refactor CarModel choices to use double quotes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,5 @@
-from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
 
 class CarMake(models.Model):
@@ -13,18 +12,21 @@ class CarMake(models.Model):
 
 class CarModel(models.Model):
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
     ]
+
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+            MinValueValidator(2015),
+        ],
+    )
     dealer_id = models.IntegerField()
 
     def __str__(self):
